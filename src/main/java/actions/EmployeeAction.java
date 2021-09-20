62db8ca0d194a5e91b9e00a2fd18a146f5d35195
kouta <>
2021-09-20 20:36:33 +0900

    従業員newアクションの作成

--- src/main/java/actions/EmployeeAction.java
+++ src/main/java/actions/EmployeeAction.java
@@ -38,6 +38,7 @@ public class EmployeeAction extends ActionBase {
      * @throws ServletException
      * @throws IOException
      */
+
     public void index() throws ServletException, IOException {
 
         //指定されたページ数の一覧画面に表示するデータを取得
@@ -61,7 +62,14 @@ public class EmployeeAction extends ActionBase {
 
         //一覧画面を表示
         forward(ForwardConst.FW_EMP_INDEX);
+    }
+    public void entryNew() throws ServletException, IOException {
 
+        putRequestScope(AttributeConst.TOKEN, getTokenId()); //CSRF対策用トークン
+        putRequestScope(AttributeConst.EMPLOYEE, new EmployeeView()); //空の従業員インスタンス
+
+        //新規登録画面を表示
+        forward(ForwardConst.FW_EMP_NEW);
     }
 
 }
